c04859e91232fb4a4b35b0ba840b8e9b07817d29
kenta okamura <side.junktown@gmail.com>
2021-08-15 18:59:02 +0900

    Add emoji collection

--- flutter_emoji_mania/lib/main.dart
+++ flutter_emoji_mania/lib/main.dart
@@ -5,109 +5,203 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade200,
+        body: SafeArea(child: RootPage()),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class RootPage extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return GridView.count(
+      primary: true,
+      padding: const EdgeInsets.all(18),
+      crossAxisSpacing: 12,
+      mainAxisSpacing: 12,
+      crossAxisCount: 2,
+      children: <Widget>[
+        EmojiCell(
+          emoji: "😀",
+          title: "Smile",
+        ),
+        EmojiCell(
+          emoji: "🗻",
+          title: "Mountain",
+        ),
+        EmojiCell(
+          emoji: "🥛",
+          title: "Milk",
+        ),
+        EmojiCell(
+          emoji: "🌵",
+          title: "Cactus",
+        ),
+        EmojiCell(
+          emoji: "🐈",
+          title: "Cat",
+        ),
+        EmojiCell(
+          emoji: "👻",
+          title: "Ghost",
+        ),
+        EmojiCell(
+          emoji: "🤖",
+          title: "Robot",
+        ),
+        EmojiCell(
+          emoji: "🚁",
+          title: "Helicopter",
+        ),
+        EmojiCell(
+          emoji: "📛",
+          title: "Tofu on fire",
+        ),
+        EmojiCell(
+          emoji: "🍕",
+          title: "Pizza",
+        ),
+        EmojiCell(
+          emoji: "🍎",
+          title: "Apple",
+        ),
+        EmojiCell(
+          emoji: "💤",
+          title: "zzz",
+        ),
+        EmojiCell(
+          emoji: "😇",
+          title: "Innocent",
+        ),
+        EmojiCell(
+          emoji: "🤪",
+          title: "Crazy",
+        ),
+        EmojiCell(
+          emoji: "👀",
+          title: "Eyes",
+        ),
+        EmojiCell(
+          emoji: "😡",
+          title: "Oko",
+        ),
+        EmojiCell(
+          emoji: "✏️",
+          title: "Pen",
+        ),
+        EmojiCell(
+          emoji: "🦄",
+          title: "Unicorn",
+        ),
+        EmojiCell(
+          emoji: "🗿",
+          title: "Moai",
+        ),
+        EmojiCell(
+          emoji: "🍄️",
+          title: "Kinoko",
+        ),
+        EmojiCell(
+          emoji: "🍣",
+          title: "Sushi",
+        ),
+        EmojiCell(
+          emoji: "🦀💥🦀",
+          title: "Kani no Kenka",
+        ),
+        EmojiCell(
+          emoji: "👨‍👩‍👧‍👧",
+          title: "Family",
+        ),
+        EmojiCell(
+          emoji: "🍵‍‍",
+          title: "Tea",
+        ),
+        EmojiCell(
+          emoji: "🍌",
+          title: "Banana",
+        ),
+        EmojiCell(
+          emoji: "⚔️",
+          title: "Duel",
+        ),
+        EmojiCell(
+          emoji: "💡",
+          title: "Idea",
+        ),
+        EmojiCell(
+          emoji: "🗝",
+          title: "Key",
+        ),
+        EmojiCell(
+          emoji: "🍆",
+          title: "Eggplant",
+        ),
+        EmojiCell(
+          emoji: "⭐️",
+          title: "Star",
+        ),
+        EmojiCell(
+          emoji: "🍖",
+          title: "Manganiku",
+        ),
+        EmojiCell(
+          emoji: "🚩",
+          title: "Flag",
+        ),
+      ],
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class EmojiCell extends StatelessWidget {
+  final String emoji;
+  final String title;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const EmojiCell({Key? key, required this.emoji, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: const BorderRadius.all(Radius.elliptical(8, 8)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.lightBlue.withAlpha(110),
+            blurRadius: 4,
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Center(
+            child: Text(
+              emoji,
+              style: TextStyle(fontSize: 36, shadows: [
+                Shadow(
+                  color: Colors.green.withAlpha(170),
+                  blurRadius: 6,
+                ),
+              ]),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+          Text(
+            title,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w800),
+          ),
+          Text(List.filled(10, title.toLowerCase()).join(" "), maxLines: 3, overflow: TextOverflow.fade),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
